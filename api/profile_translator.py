a9c5eda5dbe1be320e0aec6f8cb36732cdf20f16
Zxilly <zhouxinyu1001@gmail.com>
2020-07-22 15:54:04 +0800

    arrange

--- api/profile_translator.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('profile.json','r+') as f:
-    profile = json.loads(f.read())
-
-with open('translated_gamedata.json','r+') as f:
-    gamedata = json.loads(f.read())
-
-data = profile["data"]
